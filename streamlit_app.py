64ba57520f151c761b42557574e9b61c525ffa9c
Banda-Manisha <banda.manisha@kasmodigital.com>
2025-08-21 11:56:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,17 +35,17 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df  = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         #st.write(ingredients_string)
-        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
-        #st.write(my_insert_stmt)
-        #st.stop()
+    #st.write(my_insert_stmt)
+    #st.stop()
 
-        #st.write(my_insert_stmt)
-        time_to_insert = st.button('Submit Order')
-        if time_to_insert:
-          session.sql(my_insert_stmt).collect()
+    #st.write(my_insert_stmt)
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
         
-          st.success('Your Smoothie is ordered!', icon="✅")
+        st.success('Your Smoothie is ordered!', icon="✅")
 
 
 
